9eb656c96bac44b7865d94bf6a3f7e5395f11732
serita2 <serita2@mss-g.co.jp>
2022-11-18 17:52:21 +0900

    20221118 finish

---
+++ public/training/sample.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="sample-tag">
+      <h1>{{ title }}</h1>
+      <p>{{ msg }}</p>
+    </div>
+</template>
+  
+<script>
+  export default {
+    name: 'SampleTag',
+    data () {
+      // テンプレート内で保有するデータ
+      return {
+        title: "Vue Template File"
+      }
+    },
+    props: {
+      // 親要素から受け取るプロパティの定義
+      msg: String
+    },
+    computed: {
+      // 算出プロパティ(処理結果がキャッシュされ、入力値が変更されたときのみ再計算)
+    },
+    methods: {
+      // メソッド(毎回呼び出す度に処理を実行)
+    }
+  }
+</script>
+<!--scopedでcssの適用範囲を限定的に-->
+<style scoped>
+    p {
+      color: red;
+    }
+</style>
